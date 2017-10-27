03895a5b957c10edb35960a5d67dd1e528b9d6f1
DESKTOP-KDOD9LL\HentioR <malassigne.thomas@gmail.com>
2017-10-27 11:44:49 +0200

    bootstrap

--- index.php
+++ index.php
@@ -3,3 +3,12 @@
 ?>
 
 <title>doURBEX - Accueil</title>
+
+<div class="container">
+  <div class="col-md-6">
+    <p>Test</p>
+  </div>
+  <div class="col-md-6">
+    <p>Test2</p>
+    <button>Test</button>
+  </div>
